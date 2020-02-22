6b3a99d2ed48cb2ec921801ed737ed0cc936f589
Andoni Michael <andoni.m.garcia@gmail.com>
2020-02-21 19:48:28 -0800

    Convert wordlist into datastrctures
    Convert the generated wordlist into two frequency-based datastructures
    that will be important for later model generation. Also, limit the
    size of the considered vocabulary with a new command line argument:
    --vocabsize that must be a positive integer.

--- rapgen/argparser.py
+++ rapgen/argparser.py
@@ -1,8 +1,20 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentTypeError
 
 
 def create_arg_parser():
-    """ Creates an argument parser that handles input and output file names. """
+    """ Creates an argument parser that handles input and output file names and vocabulary size. """
+
+    def positive_integer(input_string):
+        """ Validates that the input string is a positive integer. """
+        try:
+            value = int(input_string)
+            if value > 0:
+                return value
+            else:
+                raise ArgumentTypeError('Error: {} is not a positive integer'.format(input_string))
+        except ValueError:
+            raise ArgumentTypeError('Error: {} is not a valid integer'.format(input_string))
+
     cmd_line_parser = ArgumentParser()
     cmd_line_parser.add_argument('-i',
                                  '--input',
@@ -14,4 +26,10 @@ def create_arg_parser():
                                  dest='output_file',
                                  help='The output file',
                                  required=True)
+    cmd_line_parser.add_argument('--vocabsize',
+                                 type=positive_integer,
+                                 dest='vocab_size',
+                                 help='The size of the model\'s vocabulary',
+                                 default=50000,
+                                 required=False)
     return cmd_line_parser
--- rapgen/rapgen.py
+++ rapgen/rapgen.py
@@ -1,11 +1,14 @@
 from argparser import create_arg_parser
-from data_reader import read_data
+from data_handler import build_dataset, read_data
 
 if __name__ == '__main__':
     parser = create_arg_parser()
     args = parser.parse_args()
 
-    data = read_data(args.input_file)
+    wordlist = read_data(args.input_file)
+    words_and_frequency, words_to_index = build_dataset(wordlist, args.vocab_size)
 
     with open(args.output_file, 'w') as output_file:
-        output_file.write(str(data))
+        output_file.write(str(wordlist) + '\n')
+        output_file.write(str(words_and_frequency) + '\n')
+        output_file.write(str(words_to_index) + '\n')
