7b03b601c9e49b1649a369689bc785d5e894dcef
Andoni Michael <andoni.m.garcia@gmail.com>
2020-02-23 12:16:46 -0800

    Consider bigram phrases in addition to single words
    By extending the generator to leverage Gensim's Phrase module, we are
    able to consider 2-word phrases as individual tokens, gaining better
    coherency in our comparison structures.

--- rapgen/rapgen.py
+++ rapgen/rapgen.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     data_reader = DirectoryReader(args.input_directory)
-    model = gensim.models.Word2Vec(data_reader,
+    words_and_phrases = gensim.models.Phrases(data_reader, max_vocab_size=args.vocab_size)
+    model = gensim.models.Word2Vec(words_and_phrases[data_reader],
                                    size=args.dimensions,
                                    window=args.window_size,
                                    min_count=args.min_count,
@@ -19,9 +20,8 @@ if __name__ == '__main__':
                                    sg=1)
     model.save(args.output_file)
 
-    wv = model.wv
+    word_vectors = model.wv
 
-    vocab_stats = [(word, vocab.count, vocab.index) for word, vocab in wv.vocab.items()]
-    logging.info(vocab_stats[:10])
+    vocab_stats = [(word, vocab.count, vocab.index) for word, vocab in word_vectors.vocab.items()]
     for word, _, _ in vocab_stats[:10]:
-        logging.info(wv.most_similar(word))
+        logging.info(f'{word}: {word_vectors.most_similar(word)}')
